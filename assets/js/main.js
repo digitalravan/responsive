3c9d70358f964f0359b9b75284a62c2156a43c7b
Abhay singh Rajawat <149522051+digitalravan@users.noreply.github.com>
2026-03-27 01:39:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,4 +54,69 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+
+const popup = document.getElementById("popup");
+const popupImg = document.getElementById("popup-img");
+const closeBtn = document.getElementById("close");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const popup = document.getElementById("popup");
+  const popupImg = document.getElementById("popup-img");
+  const closeBtn = document.getElementById("close");
+
+  document.querySelectorAll(".work__img").forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      popup.style.display = "flex";
+      popupImg.src = this.href;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+
+});
+
+const textElement = document.getElementById("typing");
+
+const words = ["Digital Ravan", "Abhay Singh"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  let currentWord = words[wordIndex];
+  
+  if (isDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  textElement.textContent = currentWord.substring(0, charIndex);
+
+  // color change
+  if (currentWord === "Abhay Singh") {
+    textElement.classList.add("red-text");
+  } else {
+    textElement.classList.remove("red-text");
+  }
+
+  let speed = isDeleting ? 50 : 100;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    speed = 1500; // pause
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    speed = 500;
+  }
+
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
